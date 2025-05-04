de26775d589e0db90e045002fe9ca351e80c906c
Liodakis <mliodakis@travelstaytion.com>
2025-05-04 10:38:22 +0300

    chore: initialize env config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    env: {
+        CROWDIN_API_TOKEN: process.env.CROWDIN_API_TOKEN,
+        CROWDIN_PROJECT_ID: process.env.CROWDIN_PROJECT_ID
+    },
 };
 
 export default nextConfig;
