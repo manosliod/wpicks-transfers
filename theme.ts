e4658aa1b14979f2dca10ff1c4b395b950e35d22
Liodakis <mliodakis@travelstaytion.com>
2025-05-04 18:23:12 +0300

    feat: add mui list item button style overrides

--- theme.ts
+++ theme.ts
@@ -12,6 +12,37 @@ const theme = createTheme({
       letterSpacing: '0px',
     },
   },
+  components: {
+    MuiListItemButton: {
+      styleOverrides: {
+        root: {
+          fontWeight: 700,
+          transition: 'background-color 0.3s ease, color 0.3s ease',
+          '&:hover': {
+            backgroundColor: '#f5f5f5',
+          },
+          '&.Mui-selected': {
+            backgroundColor: '#50D8A5',
+            color: '#fff',
+            '& svg': {
+              color: '#fff',
+            },
+            '&.submenu-item': {
+              backgroundColor: '#E5F9F2',
+              color: '#50D8A5',
+            },
+          },
+          '&.Mui-selected:hover': {
+            backgroundColor: '#50D8A5',
+            '&.submenu-item': {
+              backgroundColor: '#E5F9F2',
+              color: '#50D8A5',
+            },
+          },
+        },
+      },
+    },
+  },
 });
 
 export default theme;
