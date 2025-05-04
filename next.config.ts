5d04a278683cd0707e5c86e7226a6704ebbd81d6
Liodakis <mliodakis@travelstaytion.com>
2025-05-04 11:31:48 +0300

    chore: add new env var

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     env: {
+        API_BASE_URL: process.env.API_BASE_URL,
         CROWDIN_API_TOKEN: process.env.CROWDIN_API_TOKEN,
         CROWDIN_PROJECT_ID: process.env.CROWDIN_PROJECT_ID
     },
