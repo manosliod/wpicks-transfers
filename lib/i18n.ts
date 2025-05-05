717be7059143eb443a0f48e975e6769cc79134d2
Liodakis <mliodakis@travelstaytion.com>
2025-05-05 14:14:13 +0300

    refactor: put stores, utils in a shared folder in app

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -1,7 +1,7 @@
 import i18next from 'i18next';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
-import { fetchCrowdinTranslation } from '@/utils/fetchCrowdinTranslation';
+import { fetchCrowdinTranslation } from '@/app/shared/utils/fetchCrowdinTranslation';
 
 i18next
   .use(initReactI18next)
