c1a43e288b80f42141f0c77736509124b5bdd885
Liodakis <mliodakis@travelstaytion.com>
2025-05-06 22:56:54 +0300

    feat: implement transfer list mobile card

--- app/components/transfers-list/TransfersListMobile.tsx
+++ app/components/transfers-list/TransfersListMobile.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import type { FormattedTransfers } from '@/app/components/transfers-list/TransfersListTableBody';
 import WpIcon from '@/app/components/WpIcon';
+import TransfersListMobileCard from '@/app/components/transfers-list/TransfersListMobileCard';
 
 const TransfersListMobile: React.FC<{
   formattedTransfersList: FormattedTransfers[];
@@ -17,29 +18,41 @@ const TransfersListMobile: React.FC<{
     >
       {formattedTransfersList.map(({ formattedDate, items }, index) => (
         <Box
-          sx={{
-            background: '#2D3B4E0A',
-            borderRadius: '32px',
-            padding: '8px 12px',
-            minHeight: '32px',
-            flexGrow: 1,
-            flexShrink: 1,
-            display: 'flex',
-            alignItems: 'center',
-            gap: '10px',
-          }}
+          key={`mobile-${index}`}
+          sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}
         >
-          <WpIcon name="calendar" />
-          <Typography
-            variant="subtitle1"
-            fontWeight="bold"
+          <Box
             sx={{
-              color: '#2D3B4E66',
-              width: '100%',
+              background: '#2D3B4E0A',
+              borderRadius: '32px',
+              padding: '8px 12px',
+              minHeight: '32px',
+              flexGrow: 1,
+              flexShrink: 1,
+              display: 'flex',
+              alignItems: 'center',
+              gap: '10px',
             }}
           >
-            {formattedDate}
-          </Typography>
+            <WpIcon name="calendar" />
+            <Typography
+              variant="subtitle1"
+              fontWeight="bold"
+              sx={{
+                color: '#2D3B4E66',
+                width: '100%',
+              }}
+            >
+              {formattedDate}
+            </Typography>
+          </Box>
+
+          {items.map((item, index) => (
+            <TransfersListMobileCard
+              key={`mobile-transfer-${index}`}
+              transfer={item}
+            />
+          ))}
         </Box>
       ))}
     </Box>
