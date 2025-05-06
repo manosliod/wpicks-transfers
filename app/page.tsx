329f25f9e30c53fb6ef691dd62590619f3e5de9d
Liodakis <mliodakis@travelstaytion.com>
2025-05-06 17:16:14 +0300

    Revert "chore: test for deployment"
    This reverts commit b3605e2fdd279f5f33da9e9e7c5a7edc2e737a53.

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,27 @@ import TransfersList from '@/app/components/transfers-list/TransfersList';
 
 export default async function Home() {
   console.log(process.env.API_BASE_URL);
-  // const [transfersListRes, transfersDetailsRes] = await Promise.all([
-  //   fetch(process.env.API_BASE_URL + '/api/transfers-list', {
-  //     next: { revalidate: 60 },
-  //   }),
-  //   fetch(process.env.API_BASE_URL + '/api/transfers-details', {
-  //     next: { revalidate: 60 },
-  //   }),
-  // ]);
-  //
-  // const transfersListData = await transfersListRes.json();
-  // const transfersDetailsData = await transfersDetailsRes.json();
+  const [transfersListRes, transfersDetailsRes] = await Promise.all([
+    fetch(process.env.API_BASE_URL + '/api/transfers-list', {
+      next: { revalidate: 60 },
+    }),
+    fetch(process.env.API_BASE_URL + '/api/transfers-details', {
+      next: { revalidate: 60 },
+    }),
+  ]);
+
+  const transfersListData = await transfersListRes.json();
+  const transfersDetailsData = await transfersDetailsRes.json();
 
   return (
     <>
       <ClientHome>
         <BaseHeader />
         <BaseLayout>
-          <TransfersList transfersListData={null} transfersDetailsData={null} />
+          <TransfersList
+            transfersListData={transfersListData}
+            transfersDetailsData={transfersDetailsData}
+          />
         </BaseLayout>
       </ClientHome>
     </>
