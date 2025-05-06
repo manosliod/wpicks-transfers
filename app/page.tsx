27f635f4c4cbc3146d70c702f9910fc8e81f238f
Liodakis <mliodakis@travelstaytion.com>
2025-05-06 18:46:35 +0300

    refactor: add suspense for page loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,37 @@
-export const dynamic = 'auto';
+export const dynamic = 'force-dynamic';
 
-import Ssr from '@/app/components/Ssr';
+import { Suspense } from 'react';
+import Loading from '@/app/loading';
 import BaseHeader from '@/app/components/BaseHeader';
 import BaseLayout from '@/app/components/BaseLayout';
 import ClientHome from '@/app/components/ClientHome';
 import TransfersList from '@/app/components/transfers-list/TransfersList';
 
 export default async function Home() {
+  console.log(process.env.API_BASE_URL);
+  const [transfersListRes, transfersDetailsRes] = await Promise.all([
+    fetch(process.env.API_BASE_URL + '/api/transfers-list', {
+      next: { revalidate: 60 },
+    }),
+    fetch(process.env.API_BASE_URL + '/api/transfers-details', {
+      next: { revalidate: 60 },
+    }),
+  ]);
+
+  const transfersListData = await transfersListRes.json();
+  const transfersDetailsData = await transfersDetailsRes.json();
+
   return (
-    <Ssr>
+    <Suspense fallback={<Loading />}>
       <ClientHome>
         <BaseHeader />
         <BaseLayout>
-          <TransfersList />
+          <TransfersList
+            transfersListData={transfersListData}
+            transfersDetailsData={transfersDetailsData}
+          />
         </BaseLayout>
       </ClientHome>
-    </Ssr>
+    </Suspense>
   );
 }
