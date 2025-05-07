1c00d79d330f941e7be37a626839b59acd3ae8db
Liodakis <mliodakis@travelstaytion.com>
2025-05-07 20:59:42 +0300

    fix: icon props

--- app/components/icons/ArrowDottedIcon.tsx
+++ app/components/icons/ArrowDottedIcon.tsx
@@ -7,14 +7,14 @@ export function ArrowDottedIcon() {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_2_1918)">
+      <g clipPath="url(#clip0_2_1918)">
         <circle cx="3" cy="9" r="1" fill="#48D9A4" />
         <circle cx="3" cy="5" r="1" fill="#48D9A4" />
         <circle cx="3" cy="1" r="1" fill="#48D9A4" />
         <circle cx="3" cy="13" r="1" fill="#48D9A4" />
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M3 20.5L5.5 16H0.5L3 20.5Z"
           fill="#48D9A4"
         />
